9a2ba1abb8a432ef3b8224af09c2889b15f0b23a
Saya <lusy.90@mail.ru>
2022-06-02 16:38:45 +0400

    Добавлена авторизация под разными пользователями

--- Old/main.py
+++ Old/main.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from links import *
+from authorization import *
 from edit_trades import *
 from buttons import *
 from change_status import *
@@ -42,12 +43,14 @@ class PythonOrgSearch(unittest.TestCase):
 
     def test_create_trade1(self):
         # Запуск Chromedriver
-        driver = self.cd
+        #driver = self.cd
         _links = Links
         _buttons = Buttons
         _edit_trades = Edit_trades
         _edit_constructor = Edit_constructor
         _change_status = Change_status
+        _authorization = Authorization()
+        driver = _authorization.chromedriver
         _trade_links = {}
 
         # Открытие страницы авторизации
@@ -62,6 +65,9 @@ class PythonOrgSearch(unittest.TestCase):
         driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/header/div/div/div[4]/button/div').click()
         time.sleep(2)
         driver.find_element(By.CSS_SELECTOR, 'div#gatsby-focus-wrapper button[type="button"].flex.items-center.justify-center.text-n1.border-2.px-6.button-modules--container--267eH.header-modules--exitButton--2sVif.button-modules--secondary--I_OvC').click()
+        time.sleep(3)
+        _authorization.login_ft('glazovaft5@yandex.ru', 'Password123#')
+        driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/header/div/div/div[4]/button/div').click()
         time.sleep(3)
 
     #def test_search_in_python_org(self):
---
+++ authorization.py
@@ -0,0 +1,51 @@
+# -- coding: utf-8 --
+import time
+import datetime
+import unittest
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from links import *
+from edit_trades import *
+from buttons import *
+from change_status import *
+import json
+import requests
+from selenium.webdriver.common.keys import Keys
+
+class Authorization:
+    def __init__(self):
+        ser = Service('chromedriver.exe')
+        op = webdriver.ChromeOptions()
+        self.chromedriver = webdriver.Chrome(service=ser, options=op)
+
+    #Логаут(деавторизация)
+    def logout(self):
+        self.chromedriver.find_element(By.CSS_SELECTOR, '#root > div > div > div > div.sidebar > div > div.ant-layout-sider-children > aside > a').click()
+        time.sleep(2)
+        self.chromedriver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/header/div/div/div[4]/button/div').click()
+        time.sleep(2)
+        self.chromedriver.find_element(By.CSS_SELECTOR, 'div#gatsby-focus-wrapper button[type="button"].flex.items-center.justify-center.text-n1.border-2.px-6.button-modules--container--267eH.header-modules--exitButton--2sVif.button-modules--secondary--I_OvC').click()
+        time.sleep(3)
+
+    # Открытие страницы авторизации
+    def open_login_page(self):
+        self.chromedriver.get("https://stage.www.vtbconnect.ru/login")
+
+
+    # Авторизация
+    def login(self, email, password):
+        #self.assertIn(u"Авторизация", self.chromedriver.title)
+        assert u"Авторизация" in self.chromedriver.title
+        elem = self.chromedriver.find_elements(By.CSS_SELECTOR, '[type="text"][class="ant-input ant-input-lg"]')
+        elem[0].send_keys(email)
+        elem1 = self.chromedriver.find_element(By.CSS_SELECTOR, '[type="password"][class="ant-input ant-input-lg"]')
+        elem1.send_keys(password)
+        self.chromedriver.find_element(By.CSS_SELECTOR, '[type="submit"][class*="ant-btn Login__form-button"]').click()
+        time.sleep(3)
+        print('login OK')
+
+    # Авторизация под glazovaft5@yandex.ru, чтобы вызвать _authorization.login_ft('glazovaft5@yandex.ru', 'Password123#')
+    def login_ft(self, email, password):
+        self.login(email=email, password=password)
